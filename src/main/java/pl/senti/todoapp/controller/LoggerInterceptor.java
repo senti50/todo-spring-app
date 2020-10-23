063456c9cede2c71828fea3183efea60384520fc
senti50 <46302409+senti50@users.noreply.github.com>
2020-10-23 17:14:57 +0200

    added filter and interceptor which log queries

---
+++ src/main/java/pl/senti/todoapp/controller/LoggerInterceptor.java
@@ -0,0 +1,21 @@
+package pl.senti.todoapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+public static final Logger logger= LoggerFactory.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("[preHandle]"+request.getMethod()+" "+request.getRequestURI());
+        return true;
+    }
+
+}
---
+++ src/main/java/pl/senti/todoapp/controller/MvcConfiguration.java
@@ -0,0 +1,24 @@
+package pl.senti.todoapp.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Set;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    private final Set<HandlerInterceptor> interceptors;
+
+    public MvcConfiguration(final Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        interceptors.forEach(registry::addInterceptor);
+
+    }
+}
