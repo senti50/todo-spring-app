9045aa4c6a4072fba166e0869f990a7b7264bdb3
senti50 <46302409+senti50@users.noreply.github.com>
2020-10-23 23:28:49 +0200

    added logout function

--- src/main/java/pl/senti/todoapp/controller/InfoController.java
+++ src/main/java/pl/senti/todoapp/controller/InfoController.java
@@ -1,12 +1,13 @@
 package pl.senti.todoapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.senti.todoapp.TaskConfigurationProperties;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/info")
@@ -21,11 +22,13 @@ public class InfoController {
         this.myProp = myProp;
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/url")
     String url() {
         return dataSourceProperties.getUrl();
     }
 
+    @RolesAllowed({"ROLE_ADMIN","ROLE_USER"})
     @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
--- src/main/java/pl/senti/todoapp/security/SecurityConfiguration.java
+++ src/main/java/pl/senti/todoapp/security/SecurityConfiguration.java
@@ -7,11 +7,18 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+@EnableGlobalMethodSecurity(
+        jsr250Enabled = true,
+        securedEnabled = true,
+        prePostEnabled = true
+)
 @KeycloakConfiguration
 class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
@@ -33,5 +40,18 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.anonymous().disable();
+        http.authorizeRequests()
+//                .antMatchers("/info/*")
+//                .hasRole("USER")
+//                .antMatchers("/projects/*")
+//                .hasRole("ADMIN")
+                .anyRequest()
+                .permitAll();
     }
 }
