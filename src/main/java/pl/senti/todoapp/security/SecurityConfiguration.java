136545916f9076dd91aab7516c0fe29611054405
senti50 <46302409+senti50@users.noreply.github.com>
2020-10-23 21:37:15 +0200

    added Keycloack configuration for spring

---
+++ src/main/java/pl/senti/todoapp/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package pl.senti.todoapp.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Autowired
+    void configureGlobal(AuthenticationManagerBuilder auth){
+        var authorityMapper=new SimpleAuthorityMapper();
+        authorityMapper.setPrefix("ROLE_");
+        authorityMapper.setConvertToUpperCase(true);
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider=keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(authorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
